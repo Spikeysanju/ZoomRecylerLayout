efae1c5023bdb7a5bdf23573e063758ead57f56d
Sanju S <spikeysanju98@gmail.com>
2020-01-04 14:37:26 +0530

    Initial commit

---
+++ app/src/main/java/www/sanju/zoomrecylerlayout/MainActivity.kt
@@ -0,0 +1,17 @@
+package www.sanju.zoomrecylerlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import www.sanju.zoomrecyclerlayout.ZoomRecyclerLayout
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
